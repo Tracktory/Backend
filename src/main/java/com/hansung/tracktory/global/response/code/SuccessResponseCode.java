3983b871f7cca70540bef0d6ef07cceb6b83d967
ThreeeJ <2171266@hansung.ac.kr>
2026-05-06 17:16:03 +0900

    init : 프로젝트 초기 설정

--- src/main/java/com/hansung/tracktory/global/response/code/SuccessResponseCode.java
+++ src/main/java/com/hansung/tracktory/global/response/code/SuccessResponseCode.java
@@ -3,14 +3,14 @@ package com.hansung.tracktory.global.response.code;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import static com.hansung.tracktory.global.constant.StaticValue.CREATED;
-import static com.hansung.tracktory.global.constant.StaticValue.OK;
+import static com.hansung.tracktory.global.constant.StaticValue.*;
 
 @Getter
 @AllArgsConstructor
 public enum SuccessResponseCode implements BaseResponseCode {
     SUCCESS_OK("GLOBAL_200", OK, "호출에 성공했습니다."),
-    SUCCESS_CREATED("GLOBAL_201", CREATED, "호출에 성공했습니다.");
+    SUCCESS_CREATED("GLOBAL_201", CREATED, "리소스가 성공적으로 생성되었습니다."),
+    SUCCESS_NO_CONTENT("GLOBAL_204", NO_CONTENT, "요청은 성공했으나 응답할 콘텐츠가 없습니다.");
 
     private final String code;
     private final int httpStatus;
