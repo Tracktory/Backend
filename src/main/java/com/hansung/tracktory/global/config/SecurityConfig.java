c25f04369fa517819e9c404f773b1a321c225b32
ThreeeJ <2171266@hansung.ac.kr>
2026-05-30 18:10:10 +0900

    fix: SecurityConfig 수정

--- src/main/java/com/hansung/tracktory/global/config/SecurityConfig.java
+++ src/main/java/com/hansung/tracktory/global/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
